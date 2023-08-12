4bc31b5f4d0dc9917150fe5d32e2248589854776
RaenonX <raenonx0710@gmail.com>
2023-08-12 00:36:06 -0500

    Fixed page building issue

--- spinach-nft-ui/src/app/account/login/page.ts
+++ spinach-nft-ui/src/app/account/login/page.ts
@@ -1,4 +1,6 @@
 import {AccountLogin} from '@spinach/next/ui/account/login/main';
 
 
+export const dynamic = 'force-dynamic';
+
 export default AccountLogin;
--- spinach-nft-ui/src/app/account/register/page.ts
+++ spinach-nft-ui/src/app/account/register/page.ts
@@ -1,4 +1,6 @@
 import {AccountRegister} from '@spinach/next/ui/account/register/main';
 
 
+export const dynamic = 'force-dynamic';
+
 export default AccountRegister;
