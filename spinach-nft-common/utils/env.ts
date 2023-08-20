a70e151ef419d571e1a4392417eed8a4d11b8095
RaenonX <raenonx0710@gmail.com>
2023-08-20 07:46:50 -0500

    Updated `isProduction()` environment checking impl

--- spinach-nft-common/utils/env.ts
+++ spinach-nft-common/utils/env.ts
@@ -3,6 +3,6 @@ import * as env from 'env-var';
 
 export const isCi = (): boolean => env.get('CI').asBool() ?? false;
 
-export const isProduction = (): boolean => process.env.NODE_ENV === 'production';
+export const isProduction = (): boolean => env.get('NODE_ENV').asString() === 'production';
 
 export const getEnvironment = (): string => isProduction() ? 'Production' : 'Development';
