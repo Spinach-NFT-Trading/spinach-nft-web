909964a245c4b531d0732e1153b787394e16fc63
RaenonX <raenonx0710@gmail.com>
2023-08-20 13:38:15 -0500

    Added `React.Suspense` in page layout

--- spinach-nft-ui/src/ui/base/layout/common.tsx
+++ spinach-nft-ui/src/ui/base/layout/common.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {Announcements} from '@spinach/next/components/announcement/main';
+import {LoadingFullScreen} from '@spinach/next/components/icons/loading';
 import {Flex} from '@spinach/next/components/layout/flex';
 import {PageLayoutProps} from '@spinach/next/ui/base/layout/type';
 import {NavBar} from '@spinach/next/ui/base/navbar/main';
@@ -11,12 +12,14 @@ type Props = PageLayoutProps & NavBarProps;
 
 export const PageLayout = ({announcement = true, children, ...props}: React.PropsWithChildren<Props>) => {
   return (
-    <main className="min-h-full w-full">
-      <NavBar {...props}/>
-      <Flex direction="col" className="gap-1.5 p-2">
-        {announcement && <Announcements/>}
-        {children}
-      </Flex>
-    </main>
+    <React.Suspense fallback={<LoadingFullScreen/>}>
+      <main className="min-h-full w-full">
+        <NavBar {...props}/>
+        <Flex direction="col" className="gap-1.5 p-2">
+          {announcement && <Announcements/>}
+          {children}
+        </Flex>
+      </main>
+    </React.Suspense>
   );
 };
