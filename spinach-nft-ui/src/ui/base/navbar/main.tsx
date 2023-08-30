423dc1d2fc914a4a47f1c16e5f1e07dbfb68babe
RaenonX <raenonx0710@gmail.com>
2023-08-30 13:30:19 -0500

    Updated navbar width on `lg` breakpoint

--- spinach-nft-ui/src/ui/base/navbar/main.tsx
+++ spinach-nft-ui/src/ui/base/navbar/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import clsx from 'clsx';
 import {getServerSession} from 'next-auth';
 
 import {Flex} from '@spinach/next/components/layout/flex';
@@ -13,7 +14,9 @@ export const NavBar = ({hideUserControl}: NavBarProps) => {
   const session = React.use(getServerSession(authOptions));
 
   return (
-    <Flex direction="row" center className="sticky top-0 z-30 gap-1.5 border-b border-b-gray-700 p-2 backdrop-blur-lg">
+    <Flex direction="row" center className={clsx(
+      'sticky top-0 z-30 gap-1.5 border-b border-b-gray-700 p-2 backdrop-blur-lg lg:px-32',
+    )}>
       <NavBarClient session={session}/>
       {!hideUserControl && <UserControl/>}
     </Flex>
