eb1db53fde902b55fe33e7c3514959c8ed7e9782
RaenonX <raenonx0710@gmail.com>
2023-08-30 03:33:01 -0500

    Updated user icon to have circle border #24
    closes #24

--- spinach-nft-ui/src/ui/base/navbar/user/profile.tsx
+++ spinach-nft-ui/src/ui/base/navbar/user/profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import UserIcon from '@heroicons/react/24/outline/UserIcon';
+import UserCircleIcon from '@heroicons/react/24/outline/UserCircleIcon';
 import Link from 'next/link';
 
 import {Flex} from '@spinach/next/components/layout/flex';
@@ -16,7 +16,7 @@ export const UserProfileButton = ({session}: UserControlCommonProps) => {
     <Link className="nav-button-text group" href="/account/profile">
       <Flex direction="row" center className="gap-1">
         <div className="transform-smooth relative h-5 w-5">
-          <UserIcon/>
+          <UserCircleIcon/>
         </div>
         <div>
           {session.user.username}
