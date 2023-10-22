130d2eccb089aa85c75c54b79a454091b1d05e5b
RaenonX <raenonx0710@gmail.com>
2023-10-22 03:18:42 -0500

    Fixed OTP generation failed

--- spinach-nft-server/src/route/auth/verify/sms/initial.ts
+++ spinach-nft-server/src/route/auth/verify/sms/initial.ts
@@ -6,10 +6,11 @@ import {
   SmsVerifyInitialResponseSchema,
 } from '@spinach/common/types/api/auth/verify/sms/initial';
 import {isApiError} from '@spinach/common/types/api/error';
-import {generateOtp} from 'spinach-nft-ui/src/utils/otp';
+import {isProduction} from '@spinach/common/utils/env';
 
 import {Server} from '@spinach/server/const';
 import {recordSmsVerifyInitialize} from '@spinach/server/controller/auth/verify/sms/initial';
+import {generateOtp} from '@spinach/server/utils/otp';
 import {toSmsOtpPayload} from '@spinach/server/utils/sms/payload';
 import {sendSms} from '@spinach/server/utils/sms/send';
 
@@ -29,14 +30,17 @@ export const addSmsVerifyInitial = () => {
       const otp = generateOtp(6);
       const {phone} = body;
 
-      const {stats} = await sendSms({
-        data: toSmsOtpPayload({phone, otp}),
-      });
-      if (!stats) {
-        return {
-          success: false,
-          error: 'smsSendFailed',
-        };
+      // Only actually sends SMS in production
+      if (isProduction()) {
+        const {stats} = await sendSms({
+          data: toSmsOtpPayload({phone, otp}),
+        });
+        if (!stats) {
+          return {
+            success: false,
+            error: 'smsSendFailed',
+          };
+        }
       }
 
       const data = await recordSmsVerifyInitialize({
