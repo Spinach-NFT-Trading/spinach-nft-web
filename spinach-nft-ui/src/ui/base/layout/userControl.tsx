66913d61c3b58dab2135b52f6cacf97f3f742540
RaenonX <raenonx0710@gmail.com>
2023-10-22 18:17:03 -0500

    Updated user control layout split

--- spinach-nft-ui/src/ui/base/layout/userControl.tsx
+++ spinach-nft-ui/src/ui/base/layout/userControl.tsx
@@ -9,10 +9,10 @@ export const UserControlLayout = ({children}: React.PropsWithChildren<{}>) => {
   return (
     <PageLayout hideUserControl>
       <Flex direction="row">
-        <Flex className="self-center">
+        <Flex className="self-center md:basis-5/12 xl:basis-4/12">
           {children}
         </Flex>
-        <div className="relative hidden h-96 w-full self-end md:block">
+        <div className="relative hidden h-[32rem] self-end md:block md:basis-7/12 xl:basis-8/12">
           <NextImage
             src="/decoration/interactive.png"
             alt="Decoration"
