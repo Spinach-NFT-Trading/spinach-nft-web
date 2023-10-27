0c59688923bb159985dcc93b76f1b393d2f1b726
RaenonX <raenonx0710@gmail.com>
2023-10-27 11:15:25 -0500

    Added ISO date string schema

---
+++ spinach-nft-common/types/common/date.ts
@@ -0,0 +1,12 @@
+import {Static, TString, Type} from '@sinclair/typebox';
+
+
+export interface TIsoDateString extends TString {
+  static: `${number}-${number}-${number}`;
+}
+
+export const IsoDateStringSchema = Type.Unsafe<Static<TIsoDateString>>(
+  Type.String({format: 'date'}),
+);
+
+export type IsoDateString = Static<typeof IsoDateStringSchema>;
