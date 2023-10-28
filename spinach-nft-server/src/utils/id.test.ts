b17e8cc7fa8b8f823e325e610d66d6256413f6a3
RaenonX <raenonx0710@gmail.com>
2023-10-27 19:44:30 -0500

    Added ID number verification util

---
+++ spinach-nft-server/src/utils/id.test.ts
@@ -0,0 +1,11 @@
+import {describe, expect, it} from '@jest/globals';
+
+import {isIdNumberValid} from '@spinach/server/utils/id';
+
+
+describe('ID number validation', () => {
+  it('validates', () => {
+    expect(isIdNumberValid('A123456789')).toBeTruthy();
+    expect(isIdNumberValid('A123456788')).toBeFalsy();
+  });
+});
---
+++ spinach-nft-server/src/utils/id.ts
@@ -0,0 +1,18 @@
+import {toSum} from 'spinach-nft-ui/src/utils/array';
+
+import {idNumberValue} from '@spinach/server/const/id';
+
+
+const idNumberWeights = [8, 7, 6, 5, 4, 3, 2, 1, 1];
+
+export const isIdNumberValid = (idNumber: string): boolean => {
+  const value = idNumberValue[idNumber[0]];
+
+  if (!value) {
+    return false;
+  }
+
+  const weightedDigits = toSum([...idNumber.slice(1)].map((char, idx) => parseInt(char) * idNumberWeights[idx]));
+
+  return (value + weightedDigits) % 10 === 0;
+};
