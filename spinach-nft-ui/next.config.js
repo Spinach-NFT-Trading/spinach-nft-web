0e7af441b82f915936d67650654ec8922fb3b643
RaenonX <raenonx0710@gmail.com>
2023-10-27 21:45:10 -0500

    Bumped dependencies

--- spinach-nft-ui/next.config.js
+++ spinach-nft-ui/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@project/spinach-nft-common'],
-  experimental: {
-    serverActions: true,
-  },
 };
 
 module.exports = nextConfig;
