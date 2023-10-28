8c33e195992d52fe6b8a21ed59546896ebe09102
RaenonX <raenonx0710@gmail.com>
2023-10-27 19:19:21 -0500

    Added `jest` usage

---
+++ spinach-nft-server/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@spinach/server(.*)$': '<rootDir>/src/$1',
+  },
+};
