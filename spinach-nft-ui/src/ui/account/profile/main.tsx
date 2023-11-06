d3733f6079dec16300abf05ef86eec7349335ab7
RaenonX <raenonx0710@gmail.com>
2023-11-06 10:49:39 -0600

    Added NFT position link in profile page

--- spinach-nft-ui/src/ui/account/profile/main.tsx
+++ spinach-nft-ui/src/ui/account/profile/main.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import MagnifyingGlassIcon from '@heroicons/react/24/outline/MagnifyingGlassIcon';
+
+import {FlexLink} from '@spinach/next/components/layout/flex/link';
+import {Grid} from '@spinach/next/components/layout/grid';
 import {ProfileLayout} from '@spinach/next/ui/base/layout/profile/main';
 
 
 export const AccountProfile = () => {
   return (
     <ProfileLayout>
-      PROFILE
+      <Grid className="grid-cols-1 gap-2 text-3xl lg:grid-cols-2">
+        <FlexLink href="/account/position" center className="button-clickable-bg h-36 gap-2">
+          <MagnifyingGlassIcon className="h-9 w-9"/>
+          NFT 持倉
+        </FlexLink>
+      </Grid>
     </ProfileLayout>
   );
 };
