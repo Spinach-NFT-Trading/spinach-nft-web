827672e05c5616b41e6b44cda8f4efc7acf2dec2
RaenonX <raenonx0710@gmail.com>
2023-11-05 20:40:20 -0600

    Added binary data conversion utils #56

---
+++ spinach-nft-common/utils/data.ts
@@ -0,0 +1,25 @@
+import {ByteArray} from '@spinach/common/types/common/data';
+
+
+export const byteArrayToDataUri = async (data: ByteArray) => {
+  const array = new Uint8Array(data);
+
+  return new Promise<string | null>((resolver) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const result = reader.result;
+
+      resolver(typeof result === 'string' ? result : null);
+    };
+    reader.readAsDataURL(new Blob([array]));
+  });
+};
+
+export const streamToUint8Array = async (stream: NodeJS.ReadableStream): Promise<Uint8Array> => {
+  return new Promise<Buffer>((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on('data', (data) => chunks.push(data instanceof Buffer ? data : Buffer.from(data)));
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+    stream.on('error', reject);
+  });
+};
