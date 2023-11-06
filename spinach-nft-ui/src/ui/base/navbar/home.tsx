90ab129aa781342d8c814feb98b94782d33eb44e
RaenonX <raenonx0710@gmail.com>
2023-11-05 21:03:04 -0600

    Updated logo

--- spinach-nft-ui/src/ui/base/navbar/home.tsx
+++ spinach-nft-ui/src/ui/base/navbar/home.tsx
@@ -9,10 +9,10 @@ import {imageGallerySizes} from '@spinach/next/styles/image';
 
 export const NavHomepage = () => {
   return (
-    <FlexLink href="/" center className={clsx(
-      'transform-smooth nav-height button-bg-hover group sticky left-0 gap-2 rounded-lg px-1.5',
+    <FlexLink href="/" className={clsx(
+      'transform-smooth nav-height button-bg-hover group sticky left-0 items-end rounded-lg px-1.5',
     )}>
-      <div className="nav-height relative w-10">
+      <div className="nav-height relative w-14">
         <NextImage
           src="/logo-half.png"
           alt="GT NFT Market"
@@ -21,7 +21,7 @@ export const NavHomepage = () => {
           className="invert-hoverable object-contain"
         />
       </div>
-      <div className="nav-height relative hidden w-72 md:block">
+      <div className="relative hidden h-8 w-24 md:block">
         <NextImage
           src="/logo-full.png"
           alt="GT NFT Market"
