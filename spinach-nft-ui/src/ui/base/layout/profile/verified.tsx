8f25bfd52d559509ca55c196b3758ea0c5d688e4
RaenonX <raenonx0710@gmail.com>
2023-11-29 01:04:37 -0600

    Updated SVG icon styling usage

--- spinach-nft-ui/src/ui/base/layout/profile/verified.tsx
+++ spinach-nft-ui/src/ui/base/layout/profile/verified.tsx
@@ -14,9 +14,7 @@ type Props = {
 export const ProfileLayoutVerificationStatus = ({verified}: Props) => {
   return (
     <Flex direction="row" className={clsx(!verified && 'text-slate-500')}>
-      <div className={clsx('relative h-6 w-6', verified && 'text-blue-500')}>
-        <CheckBadgeIcon/>
-      </div>
+      <CheckBadgeIcon className={clsx('h-6 w-6', verified && 'text-blue-500')}/>
       <div className="whitespace-nowrap">
         {verified ? '已認證' : '未認證'}
       </div>
