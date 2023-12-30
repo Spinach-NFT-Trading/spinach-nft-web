4797e842916ecc88d6f38b4a83fb3cdb14efebd9
RaenonX <raenonx0710@gmail.com>
2023-12-29 23:54:59 -0600

    Added admin or agent checker impl

--- spinach-nft-ui/src/controller/utils.ts
+++ spinach-nft-ui/src/controller/utils.ts
@@ -2,12 +2,28 @@ import {userInfoCollection} from '@spinach/common/controller/collections/user';
 import {ObjectId} from 'mongodb';
 
 
+const getUser = (userId: string | undefined) => {
+  return userInfoCollection.findOne({_id: new ObjectId(userId)});
+};
+
 export const isAdmin = async (userId: string | undefined): Promise<boolean> => {
-  return (await userInfoCollection.countDocuments({_id: new ObjectId(userId), admin: true})) > 0;
+  return !!(await getUser(userId))?.admin;
 };
 
 export const throwIfNotAdmin = async (userId: string | undefined) => {
   if (!await isAdmin(userId)) {
     throw new Error(`User ID ${userId} does not have admin privilege!`);
+  }
+};
+
+export const isAdminOrAgent = async (userId: string | undefined): Promise<boolean> => {
+  const user = await getUser(userId);
+
+  return !!user?.admin || !!user?.agent;
+};
+
+export const throwIfNotAdminOrAgent = async (userId: string | undefined) => {
+  if (!await isAdminOrAgent(userId)) {
+    throw new Error(`User ID ${userId} does not have admin privilege or is not an agent!`);
   }
 };
