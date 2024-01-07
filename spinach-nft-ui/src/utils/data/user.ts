022195b55348b18ae5bafb01da2261036c92da7d
RaenonX <raenonx0710@gmail.com>
2024-01-07 00:18:25 -0600

    Refactored `formatUserName()` to accept null data

--- spinach-nft-ui/src/utils/data/user.ts
+++ spinach-nft-ui/src/utils/data/user.ts
@@ -1,7 +1,16 @@
+import {Nullable} from '@spinach/common/types/common/typing';
 import {UserInfo} from '@spinach/common/types/common/user';
 import {BankDetails} from '@spinach/common/types/data/user/bank';
 
 
-export const formatUserName = ({username, name}: UserInfo) => `${name} (@${username})`;
+export const formatUserName = (userInfo: Nullable<UserInfo>): string | null => {
+  if (!userInfo) {
+    return null;
+  }
+
+  const {username, name} = userInfo;
+
+  return `${name} (@${username})`;
+};
 
 export const formatBankDetails = ({code, account}: Pick<BankDetails, 'code' | 'account'>) => `${code}-${account}`;
