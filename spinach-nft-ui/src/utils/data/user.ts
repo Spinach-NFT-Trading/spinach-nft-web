25d18f7e47963500303673844f3d33e60c7f1bc2
RaenonX <raenonx0710@gmail.com>
2024-01-07 01:30:33 -0600

    Added `formatUserNameFromMap()`

--- spinach-nft-ui/src/utils/data/user.ts
+++ spinach-nft-ui/src/utils/data/user.ts
@@ -1,5 +1,6 @@
+import {NftMinterAccount} from '@spinach/common/const/nft';
 import {Nullable} from '@spinach/common/types/common/typing';
-import {UserData} from '@spinach/common/types/common/user';
+import {UserData, UserDataMap} from '@spinach/common/types/common/user';
 import {BankDetails} from '@spinach/common/types/data/user/bank';
 
 
@@ -11,6 +12,21 @@ export const formatUserName = (userInfo: Nullable<UserData>): string | null => {
   const {username, name} = userInfo;
 
   return `${name} (@${username})`;
+};
+
+type FormatUserNameFromMapOpts = {
+  userDataMap: UserDataMap,
+  userId: string,
+};
+
+export const formatUserNameFromMap = ({userDataMap, userId}: FormatUserNameFromMapOpts): string | null => {
+  if (userId === NftMinterAccount.toHexString()) {
+    return 'SYSTEM';
+  }
+
+  const userData = userDataMap[userId];
+
+  return formatUserName(userData);
 };
 
 export const formatBankDetails = ({code, account}: Pick<BankDetails, 'code' | 'account'>) => `${code}-${account}`;
