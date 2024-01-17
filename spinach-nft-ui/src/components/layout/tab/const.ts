f152f7804c4941d68f329d1731dec12197421c96
RaenonX <raenonx0710@gmail.com>
2024-01-17 12:31:26 -0600

    Updated `<TabbedContent/>` styling

--- spinach-nft-ui/src/components/layout/tab/const.ts
+++ spinach-nft-ui/src/components/layout/tab/const.ts
@@ -1,5 +1,10 @@
+import clsx from 'clsx';
+
+
 export const tabbedContentStyle = {
   common: 'inline-block rounded-t-lg border-b-2 p-4',
-  inactive: 'border-transparent hover:border-gray-300 hover:text-gray-600',
-  active: 'active border-blue-600 text-blue-600',
+  inactive: clsx(
+    'transform-smooth border-transparent text-gray-300 hover:border-gray-300 hover:bg-gray-300 hover:text-gray-600',
+  ),
+  active: 'active border-blue-500 text-blue-500',
 };
