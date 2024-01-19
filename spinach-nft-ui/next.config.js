3ae67c1b24193c3fd9068d77a206b5b138d61420
RaenonX <raenonx0710@gmail.com>
2024-01-18 21:20:07 -0600

    Disabled server minification to fix the issue where images failed to upload

--- spinach-nft-ui/next.config.js
+++ spinach-nft-ui/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@project/spinach-nft-common'],
+  experimental: {
+    serverMinification: false,
+  },
 };
 
 module.exports = nextConfig;
