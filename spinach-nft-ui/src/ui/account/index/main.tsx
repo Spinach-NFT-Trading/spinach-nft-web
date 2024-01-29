05f3e1f91717c95fe56e7804453e36d84cfa4f02
RaenonX <raenonx0710@gmail.com>
2024-01-28 20:50:17 -0600

    Fixed admin link not showed

--- spinach-nft-ui/src/ui/account/index/main.tsx
+++ spinach-nft-ui/src/ui/account/index/main.tsx
@@ -19,7 +19,10 @@ export const AccountIndex = async () => {
       <Grid className="gap-2 text-3xl lg:grid-cols-2">
         <TileLink link="/account/profile" text="會員資料" icon={<MagnifyingGlassIcon/>}/>
         <TileLink link="/account/position" text="NFT 持倉" icon={<InboxIcon/>}/>
-        {session?.user.isAdmin && <TileLink link="/admin" text="會員管理" icon={<UserCircleIcon/>}/>}
+        {
+          (session?.user.isAdmin || session?.user.isAgent) &&
+          <TileLink link="/admin" text="會員管理" icon={<UserCircleIcon/>}/>
+        }
       </Grid>
     </ProfileLayout>
   );
