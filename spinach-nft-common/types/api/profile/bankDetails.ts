732a072062ef5eba50fa4c630e7c7e70eaa66c9b
RaenonX <raenonx0710@gmail.com>
2024-05-16 08:29:16 -0500

    Removed unused typings

--- spinach-nft-common/types/api/profile/bankDetails.ts
+++
@@ -1,8 +0,0 @@
-import {Static} from '@sinclair/typebox';
-
-import {UserBankDetailsSchema} from '@spinach/common/types/data/user/bank';
-
-
-export const UserAddBankDetailsRequestSchema = UserBankDetailsSchema;
-
-export type UserAddBankDetailsRequest = Static<typeof UserAddBankDetailsRequestSchema>;
