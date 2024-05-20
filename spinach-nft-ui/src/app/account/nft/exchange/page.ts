65d4e9469c81e186510599dcc71e4f22b0890cde
RaenonX <raenonx0710@gmail.com>
2024-05-20 00:02:49 -0500

    Fixed unintentional caching on `getMatchedNftExchangeRequests()`

--- spinach-nft-ui/src/app/account/nft/exchange/page.ts
+++ spinach-nft-ui/src/app/account/nft/exchange/page.ts
@@ -1,6 +1,4 @@
 import {AccountNftExchangeConfirm} from '@spinach/next/ui/account/nft/exchange/main';
 
 
-export const dynamic = 'force-dynamic';
-
 export default AccountNftExchangeConfirm;
--- spinach-nft-ui/src/controller/nft/request/match.ts
+++ spinach-nft-ui/src/controller/nft/request/match.ts
@@ -2,6 +2,7 @@
 import {nftExchangeMatchedCollection} from '@spinach/common/controller/collections/nft';
 import {userNftPositionCollection} from '@spinach/common/controller/collections/user';
 import {ObjectId} from 'mongodb';
+import {revalidatePath} from 'next/cache';
 
 import {getDataAsArray} from '@spinach/next/controller/common';
 import {ControllerRequireUserIdOpts} from '@spinach/next/controller/user/type';
@@ -13,6 +14,11 @@ type GetMatchedNftExchangeRequestsOpts = ControllerRequireUserIdOpts;
 export const getMatchedNftExchangeRequests = async ({
   executorUserId,
 }: GetMatchedNftExchangeRequestsOpts): Promise<NftExchangeMatchedModelAtClient[]> => {
+  // Force revalidating cache every time because there's a bug that
+  // when "NFT sell request received" pops, clicking on it to redirect to the confirmation page
+  // doesn't show the newly added match - possibly due to the caching mechanism
+  revalidatePath('/account/nft/exchange');
+
   const nftPositions = await userNftPositionCollection
     .find({owner: new ObjectId(executorUserId)}).map(({nftId}) => nftId)
     .toArray();
