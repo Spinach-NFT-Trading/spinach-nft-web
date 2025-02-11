a5649d9a3d64fbde0a7baf07a51e0ba5ec6bc247
RaenonX <raenonx0710@gmail.com>
2025-02-12 00:32:27 +0800

    Fixed UI i18n not provided

--- spinach-nft-ui/src/ui/account/bank/main.tsx
+++ spinach-nft-ui/src/ui/account/bank/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {I18nProvider} from '@spinach/next/components/i18n/provider';
 import {AccountAddBankClient} from '@spinach/next/ui/account/bank/client';
 import {ProfileLayout} from '@spinach/next/ui/base/layout/profile/main';
 
@@ -7,7 +8,9 @@ import {ProfileLayout} from '@spinach/next/ui/base/layout/profile/main';
 export const AccountAddBank = () => {
   return (
     <ProfileLayout>
-      <AccountAddBankClient/>
+      <I18nProvider namespaces={['UI']}>
+        <AccountAddBankClient/>
+      </I18nProvider>
     </ProfileLayout>
   );
 };
