66f1a0a5c476003cebce90245ba088c7d2f8b0d8
RaenonX <raenonx0710@gmail.com>
2025-10-29 16:28:22 +0800

    Fixed bad social styling

--- spinach-nft-ui/src/ui/home/footer/social.tsx
+++ spinach-nft-ui/src/ui/home/footer/social.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {Flex} from '@spinach/next/components/layout/flex/common';
-import {Grid} from '@spinach/next/components/layout/grid';
 import {NextImage} from '@spinach/next/components/shared/common/image/main';
 import {HomeSocialButton} from '@spinach/next/ui/home/footer/socialButton';
 
@@ -9,12 +8,10 @@ import {HomeSocialButton} from '@spinach/next/ui/home/footer/socialButton';
 export const HomeFooterSocial = () => {
   return (
     <Flex className="relative">
-      <Grid noFullWidth center className="absolute right-5 z-10 h-full">
-        <Grid className="grid-rows-4 gap-4">
-          <HomeSocialButton href="https://lin.ee/ycjqYCe" text="LINE"/>
-          <HomeSocialButton href="https://t.me/ttgg6868" text="Telegram"/>
-        </Grid>
-      </Grid>
+      <Flex noFullWidth className="absolute bottom-0 right-5 z-10 h-fit gap-4">
+        <HomeSocialButton href="https://lin.ee/ycjqYCe" text="LINE"/>
+        <HomeSocialButton href="https://t.me/ttgg6868" text="Telegram"/>
+      </Flex>
       <div className="relative h-64 sm:h-96 xl:h-[30rem]">
         <NextImage src="/social.png" alt="Banner" className="rounded-lg bg-black object-cover object-left"/>
       </div>
--- spinach-nft-ui/src/ui/home/footer/socialButton.tsx
+++ spinach-nft-ui/src/ui/home/footer/socialButton.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import {clsx} from 'clsx';
+
 import {FlexLink} from '@spinach/next/components/layout/flex/link';
+import {getToggleButtonClass} from '@spinach/next/styles/input';
 
 
 type Props = {
@@ -10,7 +13,7 @@ type Props = {
 
 export const HomeSocialButton = ({href, text}: Props) => {
   return (
-    <FlexLink href={href} className="button-clickable w-32 border-2 border-slate-100 p-2">
+    <FlexLink href={href} className={clsx('w-32 border-2 p-2', getToggleButtonClass(false))}>
       {text}
     </FlexLink>
   );
